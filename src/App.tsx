fa8306e216c189f0333f78273f28ce7638c000df
Doğukan Kalkan <dogukankalkanfrt@gmail.com>
2026-02-24 20:11:36 +0300

    chore: initial project setup with Vite+React+ TS

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+<div>
+<h1>Web Tasarimi ve Programlama</h1>
+<h2>LAB-1</h2>
+<p>Ad Soyad: [Doğukan Kalkan]</p>
+<p>Ogrenci No: [235542019]</p>
+</div>
+)
+}
+
+export default App
